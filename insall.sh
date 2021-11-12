bfcd71e5c65456903cb78e1d06c09ca8713b1cd6
vqtanh <90762595+vqtanh@users.noreply.github.com>
2021-11-12 15:49:12 +0700

    Create insall.sh

---
+++ insall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+    /usr/bin/apt update
+    /usr/bin/apt -y install apache2-utils squid3
+    touch /etc/squid/passwd
+    /bin/rm -f /etc/squid/squid.conf
+    /usr/bin/touch /etc/squid/blacklist.acl
+    /usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/serverok/squid-proxy-installer/master/squid.conf
+    /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
+    /sbin/iptables-save
+    service squid restart
+    systemctl enable squid
+    sudo /usr/bin/htpasswd -b -c /etc/squid/passwd pr0xy P@ssw0rd@134
+    sudo systemctl reload squid
